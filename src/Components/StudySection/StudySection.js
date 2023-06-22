f6f0a8e445e98c7d3912231507d4b91c7088a6f4
salsabeel noor <salsabeelnoor@gmail.com>
2023-06-22 16:21:33 +0600

    images changed

--- src/Components/StudySection/StudySection.js
+++ src/Components/StudySection/StudySection.js
@@ -63,8 +63,8 @@ const StudySection = () => {
 
             </div>
             <div className='bg-light_gray '>
-                <div className='sm:container sm:mx-auto mx-3 py-10 sm:grid sm:grid-cols-layout place-items-center'>
-                    <p className='block text-sm'>These studies occur in only a small number of people (often 20 to 80) and the main focus is to ensure the safety
+                <div className='sm:container sm:mx-auto mx-3 py-10 sm:grid sm:grid-cols-layout gap-10 place-items-center'>
+                    <p className='block text-[15px]'>These studies occur in only a small number of people (often 20 to 80) and the main focus is to ensure the safety
                         of a treatment. The researchers will test for side effects, determine how much of the treatment is safe to give,
                         and learn how the body copes with the treatment, Often, these studies will take place in healthy volunteers
                         and in one site in the world. Galapagos Phase 1 clinical trials are therefore not included on this site; however,
